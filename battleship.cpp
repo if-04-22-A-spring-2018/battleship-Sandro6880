b95d4e52432dfc493da86c2d983ba061815865b7
sandro6880 <sandro6880@gmail.com>
2018-06-09 17:52:54 +0200

    commit

--- battleship.cpp
+++ battleship.cpp
@@ -13,3 +13,41 @@
 #include <stdio.h>
 
 #include "battleship.h"
+
+
+
+void load_game(){
+
+}
+
+/**
+*** @param row The row where we get the shot.
+*** @param col The col where we get the shot.
+*** @return The value of cell indicated by row and column of the own field (filled
+*** with the values of battleship.my). If row/col point outside the field the
+*** value OutOfRange is returned.
+*/
+CellContent get_shot(int row, int col){
+
+}
+
+/**
+*** Sets the row and column of the my guesses table to the value held in the
+*** opponents field. If this is a boat all surrounding cells are marked as water.
+*** @param row The row where we place the shot
+*** @param col The columnt where we place the shot.
+*** @return True if target field is valid, i.e., inside the field, False otherwise.
+*/
+bool shoot(int row, int col){
+
+}
+
+/**
+*** @param row The row where we want to get our own guess.
+*** @param col The column where we want to get our own guess.
+*** @return The value of the table with my guesses at row and column. If row/col
+*** is outside the field OutOfRange is returned.
+*/
+CellContent get_my_guess(int row, int col){
+
+}
